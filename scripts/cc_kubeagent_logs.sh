52ac31c5051e02c1e2678cb3932328e4ef240432
jongarner <jongarner010@gmail.com>
2021-03-02 10:08:07 -0500

    Added collection of Velero custom resource details during log collection

--- scripts/cc_kubeagent_logs.sh
+++ scripts/cc_kubeagent_logs.sh
@@ -21,6 +21,16 @@ function print_footer() {
 function get_resources() {
 	print_header "Resources in the $CLOUDCASA_NAMESPACE namespace"
 	kubectl get all -n $CLOUDCASA_NAMESPACE
+	print_header "Velero BackupStorageLocation resources in the $CLOUDCASA_NAMESPACE namespace"
+	kubectl get backupstoragelocations -n $CLOUDCASA_NAMESPACE
+	print_header "Velero VolumeSnapshotLocation resources in the $CLOUDCASA_NAMESPACE namespace"
+	kubectl get volumesnapshotlocations -n $CLOUDCASA_NAMESPACE
+	print_header "Velero Backup resources in the $CLOUDCASA_NAMESPACE namespace"
+	kubectl get backups -n $CLOUDCASA_NAMESPACE
+	print_header "Velero Restore resources in the $CLOUDCASA_NAMESPACE namespace"
+	kubectl get restores -n $CLOUDCASA_NAMESPACE
+	print_header "Velero DeleteBackupRequests resources in the $CLOUDCASA_NAMESPACE namespace"
+	kubectl get deletebackuprequests -n $CLOUDCASA_NAMESPACE
 	print_footer "End of resources in the $CLOUDCASA_NAMESPACE namespace"
 }
 
@@ -28,17 +38,27 @@ function get_resources() {
 function describe_all() {
 	print_header "kubectl describe all -n $CLOUDCASA_NAMESPACE"
 	kubectl describe all -n $CLOUDCASA_NAMESPACE
+	print_header "kubectl describe backupstoragelocations -n $CLOUDCASA_NAMESPACE"
+	kubectl describe backupstoragelocations -n $CLOUDCASA_NAMESPACE
+	print_header "kubectl describe volumesnapshotlocations -n $CLOUDCASA_NAMESPACE"
+	kubectl describe volumesnapshotlocations -n $CLOUDCASA_NAMESPACE
+	print_header "kubectl describe backups -n $CLOUDCASA_NAMESPACE"
+	kubectl describe backups -n $CLOUDCASA_NAMESPACE
+	print_header "kubectl describe restores -n $CLOUDCASA_NAMESPACE"
+	kubectl describe restores -n $CLOUDCASA_NAMESPACE
+	print_header "kubectl describe deletebackuprequests -n $CLOUDCASA_NAMESPACE"
+	kubectl describe deletebackuprequests -n $CLOUDCASA_NAMESPACE
 	print_footer "End of output"
 }
 
 # Saves logs from kubeagent pod (kubeagent and Velero containers).
 function get_kubeagent_pod_logs() {
 	print_header "Start of Kubeagent logs"
-	kubectl logs -n $CLOUDCASA_NAMESPACE $KAGENT_POD kubeagent
+	kubectl logs -n $CLOUDCASA_NAMESPACE "$KAGENT_POD" kubeagent
 	print_footer "End of Kubeagent logs"
 
 	print_header "Start of Velero logs"
-	kubectl logs -n $CLOUDCASA_NAMESPACE $KAGENT_POD kubeagent-backup-helper
+	kubectl logs -n $CLOUDCASA_NAMESPACE "$KAGENT_POD" kubeagent-backup-helper
 	print_footer "End of Velero logs"
 }
 
@@ -46,7 +66,7 @@ get_resources
 
 # Get kubeagent logs only if the pod is in a running state.
 KAGENT_POD=$(kubectl get pods -n $CLOUDCASA_NAMESPACE 2>/dev/null | awk '/[kubeagent]/ {print $1}')
-if [ ! $KAGENT_POD == "" ]; then
+if [ ! "$KAGENT_POD" == "" ]; then
 	get_kubeagent_pod_logs
 fi
 
